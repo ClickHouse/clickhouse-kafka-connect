e76c346f0f93c6d4814e7c97707c9756334caee1
Mark Zitnik <mark@clickhouse.com>
2022-11-23 16:17:32 +0200

    add support for schemaless primitive,arrays,maps

--- src/main/java/com/clickhouse/kafka/connect/sink/db/ClickHouseWriter.java
+++ src/main/java/com/clickhouse/kafka/connect/sink/db/ClickHouseWriter.java
@@ -14,6 +14,8 @@ import com.clickhouse.kafka.connect.sink.db.mapping.Table;
 import com.clickhouse.kafka.connect.sink.db.mapping.Type;
 import com.clickhouse.kafka.connect.util.Mask;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.sink.SinkRecord;
@@ -130,7 +132,7 @@ public class ClickHouseWriter implements DBWriter{
                 doInsertRawBinary(records);
                 break;
             case SCHEMA_LESS:
-                doInsertSimple(records);
+                doInsertJson(records);
                 break;
         }
     }
@@ -317,6 +319,76 @@ public class ClickHouseWriter implements DBWriter{
         LOGGER.info("batchSize {} data ms {} send {}", batchSize, s2 - s1, s3 - s2);
 
     }
+
+
+    public void doInsertJson(List<Record> records) {
+        //https://devqa.io/how-to-convert-java-map-to-json/
+        Gson gson = new Gson();
+        long s1 = System.currentTimeMillis();
+        long s2 = 0;
+        long s3 = 0;
+
+        if ( records.isEmpty() )
+            return;
+        int batchSize = records.size();
+
+        Record first = records.get(0);
+        String topic = first.getTopic();
+
+
+
+        try (ClickHouseClient client = ClickHouseClient.newInstance(ClickHouseProtocol.HTTP)) {
+            ClickHouseRequest.Mutation request = client.connect(chc.getServer())
+                    .write()
+                    .table(topic)
+                    .format(ClickHouseFormat.JSONEachRow)
+                    // this is needed to get meaningful response summary
+                    .set("insert_quorum", 2)
+                    .set("send_progress_in_http_headers", 1);
+
+
+            ClickHouseConfig config = request.getConfig();
+            request.option(ClickHouseClientOption.WRITE_BUFFER_SIZE, 8192);
+
+            CompletableFuture<ClickHouseResponse> future;
+
+            try (ClickHousePipedOutputStream stream = ClickHouseDataStreamFactory.getInstance()
+                    .createPipedOutputStream(config, null)) {
+                // start the worker thread which transfer data from the input into ClickHouse
+                future = request.data(stream.getInputStream()).send();
+                // write bytes into the piped stream
+                for (Record record: records ) {
+                    Map<String, Object> data = (Map<String, Object>)record.getSinkRecord().value();
+                    java.lang.reflect.Type gsonType = new TypeToken<HashMap>(){}.getType();
+                    String gsonString = gson.toJson(data,gsonType);
+                    BinaryStreamUtils.writeBytes(stream, gsonString.getBytes());
+                }
+
+                stream.close();
+                ClickHouseResponseSummary summary;
+                s2 = System.currentTimeMillis();
+                try (ClickHouseResponse response = future.get()) {
+                    summary = response.getSummary();
+
+                    long rows = summary.getWrittenRows();
+//                    System.out.println(rows);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException();
+                }
+            } catch (Exception ce) {
+                ce.printStackTrace();
+                throw new RuntimeException();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        s3 = System.currentTimeMillis();
+        LOGGER.info("batchSize {} data ms {} send {}", batchSize, s2 - s1, s3 - s2);
+    }
+
     public void doInsertSimple(List<Record> records) {
         // TODO: here we will need to make refactor (not to use query & string , but we can make this optimization later )
         long s1 = System.currentTimeMillis();
