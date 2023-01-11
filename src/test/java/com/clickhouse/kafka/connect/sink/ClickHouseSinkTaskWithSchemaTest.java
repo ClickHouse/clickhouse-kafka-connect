37f8042517ed45f80511e7b3c057bef8bab24f0c
Mark Zitnik <mark@clickhouse.com>
2023-01-11 16:09:28 +0200

    clean up code

--- src/test/java/com/clickhouse/kafka/connect/sink/ClickHouseSinkTaskWithSchemaTest.java
+++ src/test/java/com/clickhouse/kafka/connect/sink/ClickHouseSinkTaskWithSchemaTest.java
@@ -285,6 +285,7 @@ public class ClickHouseSinkTaskWithSchemaTest {
     }
 
     @Test
+    // https://github.com/ClickHouse/clickhouse-kafka-connect/issues/33
     public void materializedViewsBug() {
         Map<String, String> props = new HashMap<>();
         props.put(ClickHouseSinkConnector.HOSTNAME, db.getHost());
@@ -300,7 +301,6 @@ public class ClickHouseSinkTaskWithSchemaTest {
         dropTable(chc, topic);
         createTable(chc, topic, "CREATE TABLE %s ( `off16` Int16, `arr` Array(String), `arr_int8` Array(Int8), `arr_int16` Array(Int16), `arr_int32` Array(Int32), `arr_int64` Array(Int64), `arr_float32` Array(Float32), `arr_float64` Array(Float64), `arr_bool` Array(Bool)  ) Engine = MergeTree ORDER BY off16");
         createTable(chc, topic + "mate", "CREATE MATERIALIZED VIEW %s ( `off16` Int16 ) Engine = MergeTree ORDER BY `off16` POPULATE AS SELECT off16 FROM m_array_string_table_test ");
-        // https://github.com/apache/kafka/blob/trunk/connect/api/src/test/java/org/apache/kafka/connect/data/StructTest.java#L95-L98
         Collection<SinkRecord> sr = createArrayType(topic, 1);
 
         ClickHouseSinkTask chst = new ClickHouseSinkTask();
