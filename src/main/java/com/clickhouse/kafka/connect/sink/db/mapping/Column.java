f330112a37a8153813c109c212c4b676784e59f6
Maciej Lopacinski <hamsterready@gmail.com>
2023-05-24 13:36:55 +0200

    LowCardinality(Nullable(String)) and LowCardinality(String) fix

--- src/main/java/com/clickhouse/kafka/connect/sink/db/mapping/Column.java
+++ src/main/java/com/clickhouse/kafka/connect/sink/db/mapping/Column.java
@@ -123,7 +123,7 @@ public class Column {
             String mapValue = val[1].trim();
             return new Column(name, type, false, dispatchPrimitive(mapKey), dispatchPrimitive(mapValue));
         } else if (valueType.startsWith("LowCardinality")) {
-            return extractColumn(name, valueType.substring("LowCardinality".length() + 1, valueType.length() - 1), true);
+            return extractColumn(name, valueType.substring("LowCardinality".length() + 1, valueType.length() - 1), isNull);
         } else if (valueType.startsWith("Nullable")) {
             return extractColumn(name, valueType.substring("Nullable".length() + 1, valueType.length() - 1), true);
         }
