415b87beeb06bf6cb75fd4fe1bc2591cd4928f1d
Sergey Chernov <sergey.chernov@clickhouse.com>
2025-11-05 20:58:00 -0800

    disabled proxy tests

--- src/test/java/com/clickhouse/kafka/connect/sink/ClickHouseSinkTaskSchemalessTest.java
+++ src/test/java/com/clickhouse/kafka/connect/sink/ClickHouseSinkTaskSchemalessTest.java
@@ -24,6 +24,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(FromVersionConditionExtension.class)
 public class ClickHouseSinkTaskSchemalessTest extends ClickHouseBase {
 
+    static {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+    }
+
     @Test
     public void primitiveTypesTest() {
         Map<String, String> props = createProps();
