f98b9501af649dac87300e2dde4776937d3a0b07
Sergey Chernov <sergey.chernov@clickhouse.com>
2026-04-16 22:04:08 -0700

    upgraded gradle in benchmark. added test with two schemas

--- src/test/java/transforms/KeyToValueTest.java
+++ src/test/java/transforms/KeyToValueTest.java
@@ -40,6 +40,35 @@ public class KeyToValueTest {
 
 
 
+    @Test
+    public void applyWithDifferentSchemasTest() {
+        try(KeyToValue<SinkRecord> keyToValue = new KeyToValue<>()) {
+            keyToValue.configure(new HashMap<>());
+
+            SinkRecord record1 = generateSampleRecord("topic1", 0, 0);
+            SinkRecord newRecord1 = keyToValue.apply(record1);
+
+            Schema schema2 = SchemaBuilder.struct()
+                    .field("string_field", Schema.STRING_SCHEMA)
+                    .build();
+            Struct valueStruct2 = new Struct(schema2)
+                    .put("string_field", "value");
+            SinkRecord record2 = new SinkRecord("topic2", 0, null, "key2", schema2, valueStruct2, 1);
+            SinkRecord newRecord2 = keyToValue.apply(record2);
+
+            Assertions.assertTrue(newRecord1.value() instanceof Struct);
+            Assertions.assertNotNull(((Struct) newRecord1.value()).get("_key"));
+            Assertions.assertNotNull(((Struct) newRecord1.value()).schema().field("off16"));
+
+            Assertions.assertTrue(newRecord2.value() instanceof Struct);
+            Assertions.assertNotNull(((Struct) newRecord2.value()).get("_key"));
+            Assertions.assertNotNull(((Struct) newRecord2.value()).schema().field("string_field"));
+            Assertions.assertNull(((Struct) newRecord2.value()).schema().field("off16"));
+
+            Assertions.assertNotEquals(((Struct) newRecord1.value()).schema(), ((Struct) newRecord2.value()).schema());
+        }
+    }
+
     private SinkRecord generateSampleRecord(String topic, int partition, long offset) {
         Schema schema = SchemaBuilder.struct()
                 .field("off16", Schema.INT16_SCHEMA)
